bb49ee92c98e66f2f0775968ddaff01e1ff2f1c0
safiyah a-yah <asafiyah@ptnicec.ac.th>
2018-11-08 18:06:46 +0700

    componentl-app-12

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, text } from '@angular/core';
+import {  AlertController, NavController,ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,54 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public alertCtrl: AlertController,
+    public actionSheet: ActionSheetController,
+    public navCtrl: NavController) {
 
   }
 
+  showAction(){
+    const acSheet = this.actionSheet.create({
+       title: 'Test Action',
+       buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    acSheet.present();
+  }
+  showAlert1(){
+    const alert = this.alertCtrl.create({
+      title: 'Confirm Answer!!',
+      subTitle: 'Safiyah a-yah',
+      buttons: [
+        {
+        text:'OK'
+        },
+        {
+          text: 'Cancel',
+          handler: () => {
+            console.log("Cancel");
+          },
+        }
+      ]
+    });
+    alert.present();
+  }
 }
